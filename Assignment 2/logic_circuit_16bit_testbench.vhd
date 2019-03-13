2627b86e3a6f4a91ac95ae7e74b27a77401e7ff4
dartse <darts@tcd.ie>
2019-03-13 17:42:56 +0000

    Update logic_circuit_16bit_testbench.vhd

--- Assignment 2/logic_circuit_16bit_testbench.vhd
+++ Assignment 2/logic_circuit_16bit_testbench.vhd
@@ -1,43 +1,50 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 03/13/2019 02:07:19 PM
--- Design Name: 
--- Module Name: logic_circuit_16bit_testbench - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity logic_circuit_16bit_testbench is
---  Port ( );
 end logic_circuit_16bit_testbench;
 
 architecture Behavioral of logic_circuit_16bit_testbench is
+    component logic_circuit_16bit 
+    port (
+        A : in STD_LOGIC_VECTOR (15 downto 0);
+        B : in STD_LOGIC_VECTOR (15 downto 0);
+        sel : in STD_LOGIC_VECTOR (1 downto 0);
+        output : out STD_LOGIC_VECTOR (15 downto 0)
+    );
+    end component;
 
+    --inputs 
+    signal A : STD_LOGIC_VECTOR (15 downto 0) := (others => '0');
+    signal B : STD_LOGIC_VECTOR (15 downto 0) := (others => '0');
+    signal sel : STD_LOGIC_VECTOR (1 downto 0) := (others => '0');
+
+    --outputs
+    signal output : STD_LOGIC_VECTOR (15 downto 0) := (others => '0');
+
+    constant wait_time : time := 30ns;
 begin
+    uut: logic_circuit_16bit port map(
+        A => A,
+        B => B,
+        sel => sel,
+        output => output
+    );
 
+    stim_proc: process
+    begin
+        A <= x"3C3C";
+        B <= x"F0F0";
+        sel <= "00";
+        wait for wait_time;
 
+        sel <= "01";
+        wait for wait_time;
+
+        sel <= "10";
+        wait for wait_time;
+
+        sel <= "11";
+        wait for wait_time;
+    end process;
 end Behavioral;
