33261f925a015873b6c8b5ee906ce04410e0b3a3
dartse <darts@tcd.ie>
2019-03-13 17:48:02 +0000

    Update arithmetic_circuit_16bit_testbench.vhd

--- Assignment 2/arithmetic_circuit_16bit_testbench.vhd
+++ Assignment 2/arithmetic_circuit_16bit_testbench.vhd
@@ -1,42 +1,24 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 03/13/2019 02:07:19 PM
--- Design Name: 
--- Module Name: arithmetic_circuit_16bit_testbench - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity arithmetic_circuit_16bit_testbench is
---  Port ( );
 end arithmetic_circuit_16bit_testbench;
 
 architecture Behavioral of arithmetic_circuit_16bit_testbench is
+    component arithmetic_circuit_16bit
+    port(
+        A : in STD_LOGIC_VECTOR (15 downto 0);
+        B : in STD_LOGIC_VECTOR (15 downto 0);
+        mode_select : in STD_LOGIC_VECTOR (1 downto 0);
+        carryIn : in STD_LOGIC;
+        output : out STD_LOGIC_VECTOR (15 downto 0);
+        carryOut : out STD_LOGIC;
+        overFlow : out STD_LOGIC
+    );
+    end component;
 
+    --inputs 
+    signal A : STD_LOGIC_VECTOR (15 downto 0) := (others => '0');
 begin
 
 
