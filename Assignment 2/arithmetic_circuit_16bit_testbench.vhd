71577e701943b4219a453cd50817a692e7f7765f
dartse <darts@tcd.ie>
2019-03-13 18:16:30 +0000

    Update arithmetic_circuit_16bit_testbench.vhd

--- Assignment 2/arithmetic_circuit_16bit_testbench.vhd
+++ Assignment 2/arithmetic_circuit_16bit_testbench.vhd
@@ -19,7 +19,30 @@ architecture Behavioral of arithmetic_circuit_16bit_testbench is
 
     --inputs 
     signal A : STD_LOGIC_VECTOR (15 downto 0) := (others => '0');
-begin
+    signal B : STD_LOGIC_VECTOR (15 downto 0) := (others => '0');
+    signal mode_select : STD_LOGIC_VECTOR (1 downto 0) := (others => '0');
+    signal carryIn : STD_LOGIC := '0';
 
+    --output
+    signal output : STD_LOGIC_VECTOR (15 downto 0) := (others => '0');
+    signal carryOut : STD_LOGIC := '0';
+    signal overFlow : STD_LOGIC := '0';
+
+    constant wait_time : time := 30ns;
+begin
+    uut: arithmetic_circuit_16bit port map(
+        A => A,
+        B => B,
+        mode_select => mode_select,
+        carryIn => carryIn,
+        output => output,
+        carryOut => carryOut,
+        overFlow => overFlow
+    );
+
+    stim_proc: process
+    begin
+        
+    end process;
 
 end Behavioral;
