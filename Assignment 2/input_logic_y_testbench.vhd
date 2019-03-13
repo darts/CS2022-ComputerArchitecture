a24339a37b66f9b3d158ae02b923510db4d260b8
dartse <darts@tcd.ie>
2019-03-13 17:25:42 +0000

    Update input_logic_y_testbench.vhd

--- Assignment 2/input_logic_y_testbench.vhd
+++ Assignment 2/input_logic_y_testbench.vhd
@@ -1,43 +1,49 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 03/13/2019 02:07:19 PM
--- Design Name: 
--- Module Name: input_logic_y_testbench - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity input_logic_y_testbench is
---  Port ( );
 end input_logic_y_testbench;
 
 architecture Behavioral of input_logic_y_testbench is
+    component input_logic_y is
+    Port (
+        B : in STD_LOGIC_VECTOR (15 downto 0);
+        sel : in STD_LOGIC_VECTOR (1 downto 0);
+        out_B : out STD_LOGIC_VECTOR (15 downto 0)
+     );
+    end component;
 
+    --inputs 
+    signal B : STD_LOGIC_VECTOR (15 downto 0) := (others => '0');
+    signal sel : STD_LOGIC_VECTOR (1 downto 0) := (others => '0');
+
+    --output
+    signal out_B : STD_LOGIC_VECTOR (15 downto 0) := (others => '0');
+
+    constant wait_time : time := 30ns;
 begin
+    uut: input_logic_y port map(
+        B => B,
+        sel => sel,
+        out_B => out_B
+    );
 
+    stim_proc: process
+        begin
+
+            B <= x"0F0F";
+            sel <= "00";
+            wait for wait_time;
+
+            sel <= "01";
+            wait for wait_time;
+
+            sel <= "10";
+            wait for wait_time;
+
+            sel <= "11";
+            wait for wait_time;
+
+    end process;
 
 end Behavioral;
