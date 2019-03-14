97c2d48810fe8657cc0db6da5eae59a1f44de708
dartse <darts@tcd.ie>
2019-03-14 16:49:39 +0000

    FU done in theory
    FU is done, needs testbench, not verified valid yet.

--- Assignment 2/FU_16bit.vhd
+++ Assignment 2/FU_16bit.vhd
@@ -26,12 +26,11 @@ architecture Behavioral of FU_16bit is
     );
     end component;
 
-    component logic_circuit_16bit
+    component shifer1_16bit
     port (
-        A : in STD_LOGIC_VECTOR (15 downto 0);
-        B : in STD_LOGIC_VECTOR (15 downto 0);
-        sel : in STD_LOGIC_VECTOR (1 downto 0);
-        output : out STD_LOGIC_VECTOR (15 downto 0)
+        inPut : in STD_LOGIC_VECTOR (15 downto 0);
+        outPut : out STD_LOGIC_VECTOR (15 downto 0);
+        sel : in STD_LOGIC_VECTOR (1 downto 0)
     );
     end component;
 
@@ -50,7 +49,40 @@ architecture Behavioral of FU_16bit is
         outPut : out STD_LOGIC 
     );
     end component;
+
+    signal aluOut, shiftOut : STD_LOGIC_VECTOR (15 downto 0);
+    constant gate_delay: Time := 1ns; 
+
 begin
 
+    alu : alu_16bit port map(
+        A => A,
+        B => B,
+        mode_select => mode_select(3 downto 0),
+        V => V,
+        C => C,
+        outPut => aluOut
+    );
+
+    shift : shifter1_16bit port map(
+        inPut => B,
+        outPut => shiftOut,
+        sel => mode_select(3 downto 2)
+    )
+
+    outSelect : mux2_16bit port map(
+        sel => mode_select(4),
+        in0 => aluOut,
+        in1 => shiftOut,
+        z => outPut
+    )
+
+    zero : zero_detect_16bit port map(
+        inPut => aluOut,
+        outPut => Z
+    )
+
+    N <= '1' after gate_delay when aluOut(15)='0' else
+        '0' after gate_delay;
 
 end Behavioral;
